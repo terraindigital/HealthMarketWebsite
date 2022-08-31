ef1e4f9bb86fdad4400b6fae2b9cea3100bab12c
Scott Thrall <sthrall@night-market.com>
2022-08-31 15:04:41 -0700

    Fixing issue with the contact footer disclaimer not pulling properly.

--- src/hooks/useContactPageQuery.tsx
+++ src/hooks/useContactPageQuery.tsx
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby";
 export const useContactPageQuery = () => {
   const data = useStaticQuery(graphql`
     query contactQuery {
-      page: wpPage(databaseId: {eq: 1067}) {
+      page: wpPage(databaseId: {eq: 12}) {
         id
         disclaimers {
           disclaimer
