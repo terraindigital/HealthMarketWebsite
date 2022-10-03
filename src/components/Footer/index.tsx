ba25d1f0b039a38dbc14b1452c403636ee640b85
verystack <59302430+verystack@users.noreply.github.com>
2022-10-04 00:57:59 +1100

    Feat: add compliance and disclaimer support to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -31,60 +31,64 @@ import Facebook from "../../static/images/facebook.webp"
 import BBB from "../../static/images/bbb-seal.png"
 
 interface Props {
-  children: ReactNode
+  complianceCode: string
+  children: string
 }
 
-const Footer: FC<Props> = ({children}) => {
+const Footer: FC<Props> = ({ complianceCode, children }) => {
   const { productMenu } = useFooterProductMenuQuery()
   const { connectMenu } = useFooterConnectMenuQuery()
   const { counselMenu } = useFooterGetCounselMenuQuery()
   const { getToKnowUsMenu } = useFooterGetToKnowUsMenuQuery()
 
   return (
-    <FooterWrapper>
-      <WidgetAreaOne>
-        <FooterLogo>
-          <Link to="/">
-            <img src={Logo} alt="footer logo" />
-          </Link>
-        </FooterLogo>
-        <SocialWidget>
-          <SocialIcon>
-            <a href="https://www.facebook.com/HealthMarkets">
-              <img src={Facebook} alt="facebook logo" />
+      <FooterWrapper>
+        <WidgetAreaOne>
+          <FooterLogo>
+            <Link to="/">
+              <img src={Logo} alt="footer logo" />
+            </Link>
+          </FooterLogo>
+          <SocialWidget>
+            <SocialIcon>
+              <a href="https://www.facebook.com/HealthMarkets">
+                <img src={Facebook} alt="facebook logo" />
+              </a>
+            </SocialIcon>
+          </SocialWidget>
+          <BBBSeal>
+            <a href="https://www.bbb.org/us/tx/north-richland-hills/profile/insurance-companies/healthmarkets-insurance-agency-0825-235973986/#sealclick">
+              <img src={BBB} alt="better business bureau seal" />
             </a>
-          </SocialIcon>
-        </SocialWidget>
-        <BBBSeal>
-          <a href="https://www.bbb.org/us/tx/north-richland-hills/profile/insurance-companies/healthmarkets-insurance-agency-0825-235973986/#sealclick">
-            <img src={BBB} alt="better business bureau seal" />
-          </a>
-        </BBBSeal>
-      </WidgetAreaOne>
-      <WidgetAreaTwo>
-        <WidgetWrapper>
-          <FooterMenuWrapper>
-            <FooterMenu menu={productMenu} title="Products" />
-            <FooterMenu menu={connectMenu} title="Connect" />
-            <FooterMenu menu={counselMenu} title="Get Counsel" />
-            <FooterMenu menu={getToKnowUsMenu} title="Get to Know Us" />
-          </FooterMenuWrapper>
-          <Disclaimer className="hide-at-device">
+          </BBBSeal>
+        </WidgetAreaOne>
+        <WidgetAreaTwo>
+          <WidgetWrapper>
+            <FooterMenuWrapper>
+              <FooterMenu menu={productMenu} title="Products" />
+              <FooterMenu menu={connectMenu} title="Connect" />
+              <FooterMenu menu={counselMenu} title="Get Counsel" />
+              <FooterMenu menu={getToKnowUsMenu} title="Get to Know Us" />
+            </FooterMenuWrapper>
+            <Disclaimer className="hide-at-device">
+              <p>&copy; 2022 HealthMarkets Insurance Agency. All rights reserved.</p>
+              <span dangerouslySetInnerHTML={{ __html: children }} />
+              <p className="text-white">{complianceCode}</p>
+            </Disclaimer>
+          </WidgetWrapper>
+          <PlansByStateMenu/>
+          <Disclaimer className="show-at-mobile">
             <p>&copy; 2022 HealthMarkets Insurance Agency. All rights reserved.</p>
-            {children}
+            <span dangerouslySetInnerHTML={{ __html: children }} />
+            <p className="text-white">{complianceCode}</p>
           </Disclaimer>
-        </WidgetWrapper>
-        <PlansByStateMenu/>
-        <Disclaimer className="show-at-mobile">
+        </WidgetAreaTwo>
+        <Disclaimer className="show-at-device hide-at-mobile">
           <p>&copy; 2022 HealthMarkets Insurance Agency. All rights reserved.</p>
-          {children}
+          <span dangerouslySetInnerHTML={{ __html: children }} />
+          <p className="text-white">{complianceCode}</p>
         </Disclaimer>
-      </WidgetAreaTwo>
-      <Disclaimer className="show-at-device hide-at-mobile">
-        <p>&copy; 2022 HealthMarkets Insurance Agency. All rights reserved.</p>
-        {children}
-      </Disclaimer>
-    </FooterWrapper>
+      </FooterWrapper>
   )
 }
 
