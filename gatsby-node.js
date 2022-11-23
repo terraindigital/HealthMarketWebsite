3068608fa346bdcb47ddc103a61d384d725e2961
Jeremy Moore <jmoore2027@gmail.com>
2022-11-22 18:20:40 -0600

    fix missing replace

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,7 +22,7 @@ exports.createPages = async ({ graphql, actions }) => {
         if (permalink) {
             const stripped = permalink.replace("https://www.healthmarkets.com", "").replace("http://www.healthmarkets.com", "").replace("https://healthmarkets.com", "").replace("http://healthmarkets.com", "").replace("/local-health-insurance-agent", "")
             if (stripped) {
-                createRedirect({ fromPath: stripped, toPath: `https://www.healthmarkets.com/local-health-insurance-agent/${slug}`, isPermanent: true, statusCode: 301, redirectInBrowser: true })
+                createRedirect({ fromPath: stripped, toPath: `https://www.healthmarkets.com/local-health-insurance-agent/${slug.replace(/\./g, '')}`, isPermanent: true, statusCode: 301, redirectInBrowser: true })
             }
         }
     })
