b0a9d18095a1b105668cf3864ce5cdee86e574ac
cienfuegos-dev <cienfuegos.dev@gmail.com>
2023-03-29 20:40:21 -0600

    Phone section

--- src/pages/medicare/medicare-prescription-drug2/index.tsx
+++ src/pages/medicare/medicare-prescription-drug2/index.tsx
@@ -5,15 +5,14 @@ import React from "react";
 import {useMedicarePrescriptionDrugPageQuery} from "../../../hooks/insurance/useMedicarePrescriptionDrugPageQuery";
 
 // Styles
-
 // Scripts
-
 // Components
 import Layout from "../../../components/Layout";
 import Footer from "../../../components/Footer";
 import {Tracing} from "../../../components/Tracing/Tracing";
 import styled from "@emotion/styled";
 import PageHeroForm from "../../../components/Hero/PageHeroForm";
+import PhoneIcon from "../../../static/images/phone-icon.png";
 
 const MedicarePrescriptionDrugPage = () => {
     const {page} = useMedicarePrescriptionDrugPageQuery();
@@ -27,7 +26,13 @@ const MedicarePrescriptionDrugPage = () => {
                     light
                     btnLeftText={page.pageHeroFields.heroButtons.heroButton1.text}
                     btnRightText={page.pageHeroFields.heroButtons.heroButton2.text}
-                    footerContent={page.pageHeroFields.callUs}/>
+                    hideFooter
+                />
+                <CallUsCtn>
+                    <img src={PhoneIcon} />
+                    <CallUsText>Call XXX-XXX-XXXX to speak to a licensed insurance agent.</CallUsText>
+                </CallUsCtn>
+                {/*<HeroDisclaimerResponsive dangerouslySetInnerHTML={{__html: page.medicarePrescriptionDrugPageCustomFields?.heroDisclaimer}}/>*/}
             </PageContainer>
             <Footer>
                 {page.disclaimers.disclaimer}
@@ -37,7 +42,29 @@ const MedicarePrescriptionDrugPage = () => {
 };
 
 const PageContainer = styled.div`
-    padding: 0 24px;
+  padding: 0 24px;
+  background-color: #E3DEDA;
+`;
+
+const CallUsCtn = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 38px;
+
+  > img {
+    width: 19px;
+    margin: 4px 15px 0 2px;
+  }
+`;
+
+const CallUsText = styled.div`
+  font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 100%;
+
+  color: #4D4D4D;
 `;
 
 const MainTitle = styled.h1`
@@ -45,7 +72,7 @@ const MainTitle = styled.h1`
 
   font-family: 'IvyPresto Display';
   font-family: 'IvyPresto Display-SemiBold', serif;;
-  
+
   font-style: normal;
   font-weight: 600;
   font-size: 50px;
@@ -60,7 +87,7 @@ const MainTitle = styled.h1`
 
 const Subtitle = styled.h2`
   margin: 0 auto 50px;
-  
+
   height: 48px;
 
   font-family: 'Open Sans', Arial, Helvetica, sans-serif;
