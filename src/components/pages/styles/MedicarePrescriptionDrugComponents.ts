344ab2d56bb33d3c287a577555d431c457539158
cienfuegos-dev <cienfuegos.dev@gmail.com>
2023-04-14 16:34:50 -0600

    Use constant

--- src/components/pages/styles/MedicarePrescriptionDrugComponents.ts
+++ src/components/pages/styles/MedicarePrescriptionDrugComponents.ts
@@ -85,7 +85,7 @@ export const SectionSubtitle = styled.h2`
   font-size: 20px;
   line-height: 130%;
 
-  color: #0065BD;
+  color: var(--color-primary-dark);
 
   padding: 0 30px;
   margin-top: 40px;
