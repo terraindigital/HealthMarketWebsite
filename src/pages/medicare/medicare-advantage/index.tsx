92d649ff3c3060118edfbfc975e71992d998a50b
Scott Thrall <sthrall@night-market.com>
2023-04-18 13:45:47 -0700

    Fixing medials to make sure we can build.

--- src/pages/medicare/medicare-advantage/index.tsx
+++ src/pages/medicare/medicare-advantage/index.tsx
@@ -14,6 +14,7 @@ import {
   MedicareAdvantageSection,
   MedicareCardText,
   MedicareCtaTitle,
+  MedicareMedial,
   PageStyles,
   HeroHeading,
   HeroSubheading
@@ -199,7 +200,7 @@ const MedicareAdvantagePage = () => {
         </a>
       </GuideCTA>
 
-      <Medial color={page.medicareAdvantagePageCustomFields.medicareAdvSection3.color}>
+      {/* <Medial color={page.medicareAdvantagePageCustomFields.medicareAdvSection3.color}>
         <div dangerouslySetInnerHTML={{ __html: page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn1.heading }} />
         <div className="button-container">
           <a href={page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn2.button.button1.link} onClick={routeLink}>
@@ -213,8 +214,22 @@ const MedicareAdvantagePage = () => {
             </Button>
           </a>
         </div>
-      </Medial>
-      
+      </Medial> */}
+      <MedicareMedial color={page.medicareAdvantagePageCustomFields.medicareAdvSection3.color}>
+        <div dangerouslySetInnerHTML={{ __html: page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn1.heading }} />
+        <div className="button-container">
+          <a href={page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn2.button.button1.link} onClick={routeLink}>
+            <Button background="accent-alt" border="light" color="light">
+              {page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn2.button.button1.text}, TTY 771
+            </Button>
+          </a>
+          <a href={page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn2.button.button2.link} onClick={routeLink}>
+            <Button background="accent-alt" border="light" color="light">
+              {page.medicareAdvantagePageCustomFields.medicareAdvSection3.medicareAdvColumns.medicareAdvColumn2.button.button2.text}
+            </Button>
+          </a>
+        </div>
+      </MedicareMedial>
       <Section
         color={page.medicareAdvantagePageCustomFields.medicareAdvSection4.color}
         heading={page.medicareAdvantagePageCustomFields.medicareAdvSection4.heading}>
