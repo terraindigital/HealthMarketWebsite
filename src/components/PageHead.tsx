b1ef9eaf1dfb3643f7404df6af09aec686e8f59f
Scott Thrall <sthrall@night-market.com>
2023-06-07 09:48:02 -0700

    Adding analytics script from client to all page heads.

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -36,6 +36,7 @@ const PageHead: FC<Props> = ({
     const canonical = site.siteMetadata.siteUrl + location.pathname;
     const siteTitle = title || site.siteMetadata.title;
     const metaDescription = description || site.siteMetadata.description;
+    const analytics = '(function(g,b,d,f){(function(a,c,d){if(a){var e=b.createElement("style");e.id=c;e.innerHTML=d;a.appendChild(e)}})(b.getElementsByTagName("head")[0],"at-body-style",d);setTimeout(function(){var a=b.getElementsByTagName("head")[0];if(a){var c=b.getElementById("at-body-style");c&&a.removeChild(c)}},f)})(window,document,"body {opacity: 0 !important}",3E3);';
 
     return (
       <>
@@ -56,6 +57,8 @@ const PageHead: FC<Props> = ({
         <meta property="twitter:creator" content={site.siteMetadata.author} />
         <meta property="twitter:title" content={siteTitle} />
         <meta property="twitter:description" content={metaDescription} />
+
+        <script dangerouslySetInnerHTML={{ __html: analytics }}></script>
       </>
     );
 }
