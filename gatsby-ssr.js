cf640cdd912c0acf360a822c10f99b7aab18ffee
verystack <59302430+verystack@users.noreply.github.com>
2023-07-01 02:52:51 +1000

    Refactor: update personalization timeout

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,7 +9,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
                 type: 'application/javascript',
                 dangerouslySetInnerHTML: {
                     __html: `
-                        (function(g,b,d,f){(function(a,c,d){if(a){var e=b.createElement("style");e.id=c;e.innerHTML=d;a.appendChild(e)}})(b.getElementsByTagName("head")[0],"at-body-style",d);setTimeout(function(){var a=b.getElementsByTagName("head")[0];if(a){var c=b.getElementById("at-body-style");c&&a.removeChild(c)}},f)})(window,document,"body {opacity: 0 !important}",3E3);
+                        (function(g,b,d,f){(function(a,c,d){if(a){var e=b.createElement("style");e.id=c;e.innerHTML=d;a.appendChild(e)}})(b.getElementsByTagName("head")[0],"at-body-style",d);setTimeout(function(){var a=b.getElementsByTagName("head")[0];if(a){var c=b.getElementById("at-body-style");c&&a.removeChild(c)}},f)})(window,document,"body {opacity: 0 !important}",500);
                     `,
                 },
             },
