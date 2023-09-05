0033805e1b26a4a487173f8797ac8c3764e5c64b
Gerardo Delao <Gerardo_delao@ymail.com>
2023-09-05 16:01:42 -0600

    ready for review

---
+++ src/components/pages/styles/IndividualPageStyle.ts
@@ -0,0 +1,325 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const TierListStyles = css`
+.center-content {
+  text-align: center;
+}
+.sectionPar {
+  max-width: 800px;
+  width: 100%;
+  text-align: left;
+  margin: 0 auto;
+}
+
+.affordable-plans {
+  margin-bottom: 3rem;
+  text-align: center;
+}
+
+.affordable-plans h1 {
+  margin-bottom: 2rem;
+  color:#009FDA;
+}
+.affordable-plans p {
+  text-align: left;
+  margin: 0;
+  padding: 1rem;
+  max-width: 90rem;
+  align-self: center;
+  display: inline-block;
+}
+
+@media (max-width: 768px) {
+  .flex-container p {
+    font-size: 1.5rem;
+  }
+
+  .affordable-plans p {
+    padding-left: 20px;
+  }
+
+  .sectionPar {
+    max-width: 100%;
+    font-size: 1.5rem;
+  }
+
+  .affordable-plans h1 {
+    font-size: 2.5rem;
+  }
+
+.items {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  position: relative;
+}
+
+.items li {
+  position: relative;
+  margin-bottom: 40px;
+  padding-left: 40px;
+}
+
+.dot {
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background-color: #009FDA;
+  position: absolute;
+  left: 0;
+  top: 10px;
+}
+
+.line {
+  position: absolute;
+  width: 2px;
+  height: 187%;
+  background-color: #009FDA;
+  left: 5px;
+  top: 10px;
+  z-index: -1;
+}
+
+.items li:last-child .line {
+  display: none;
+}
+
+.items li:nth-last-child(2) .line {
+  display: none;
+}
+
+.items li:last-child .dot {
+  background-color: grey;
+}
+
+  .full-rounded {
+    margin-top: 3rem;
+  }
+
+  .full-rounded p {
+    padding: 1.2rem;
+  }
+
+  .hide-at-mobile {
+    display: none;
+  }
+  .show-at-mobile {
+    display: block;
+  }
+}
+
+@media (min-width: 769px) {
+  .show-at-mobile {
+    display: none;
+  }
+  .hide-at-mobile {
+    display: block;
+  }
+}
+
+ @media only screen and (min-width: 788px) {
+
+  .items {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+  }
+
+  .items::before {
+    content: "";
+    position: absolute;
+    top: 80px;
+    left: 50%;
+    width: 2px;
+    height: 56%;
+    background-color: #009FDA;
+    transform: translateX(-50%);
+  }
+
+  .items li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
+    margin: 20px 0;
+    padding: 20px;
+    position: relative;
+  }
+
+  .items li:nth-child(odd) {
+    margin-right: auto;
+    text-align: right;
+    top: 0;
+  }
+
+  .items li:nth-child(even) {
+    margin-left: auto;
+    text-align: left;
+    top: -115px;
+  }
+
+  .items li:nth-child(even)::before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: -10px;
+    width: 20px;
+    height: 20px;
+    background-color: #009FDA;
+    border: 2px solid #009FDA;
+    border-radius: 50%;
+    transform: translateY(-100%);
+  }
+
+  .items li:nth-child(odd) span {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+
+  .items li:nth-child(2n+1)::before {
+    background-color: grey;
+  }
+
+.items li:nth-child(4)::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: -10px;
+  width: 20px;
+  height: 20px;
+  background-color: #009FDA;
+  border: 2px solid #009FDA;
+  border-radius: 50%;
+  transform: translateY(-50%);
+}
+
+  .items li:nth-child(5)::before {
+    display: none;
+  }
+
+.items li:nth-child(10)::before {
+  background-color: grey;
+  border: grey;
+}
+    .hero > img {
+      transform: scaleX(-1);
+    }
+
+  .flexed.section p{
+    margin-top: 1rem;
+    line-height: 4rem;
+  }
+
+  .plan .affordable-plans p {
+    line-height: 4rem;
+  }
+  .full-rounded {
+  padding-bottom: 5rem;
+}
+
+.items li {
+  font-size: 2.5rem;
+}
+
+.full-rounded p {
+  text-align: center;
+  max-width: 100%;
+}
+   .footerContent {
+    font-size: 50rem;
+  }
+
+    .hide-at-mobile {
+    display: none;
+  }
+
+  .timeline {
+  position: relative;
+  margin-left: 20px;
+}
+
+.timeline ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.timeline li {
+  position: relative;
+  margin-bottom: 50px;
+}
+
+.timeline li:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: -10px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #ccc;
+}
+
+.timeline li:after {
+  content: "";
+  position: absolute;
+  top: 10px;
+  left: -5px;
+  width: 0;
+  height: 0;
+  border-top: 10px solid transparent;
+  border-bottom: 10px solid transparent;
+  border-left: 10px solid #ccc;
+}
+
+.timeline .dot {
+  position: absolute;
+  top: 0;
+  left: -20px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #ccc;
+  z-index: 1;
+}
+
+.timeline .content {
+  position: relative;
+  padding-left: 60px;
+}
+
+.timeline h2 {
+  margin-top: 0;
+}
+
+.timeline p {
+  margin-bottom: 0;
+}
+.hide-at-mobile {
+  display: block;
+}
+
+/* Hidden by default, show on mobile */
+.show-at-mobile {
+  display: none;
+}
+}
+
+`;
+
+export const HeroHeading = styled.h1`
+  color: #4D4D4D;
+  margin-bottom: 2.4rem;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.15);
+`;
+
+export const HeroSubheading = styled.h4`
+  color: #4D4D4D;
+  font-family: var(--font-body);
+  margin-bottom: 4rem;
+
+`;
---
+++ src/pages/health-insurance/individual-page/index.tsx
@@ -0,0 +1,229 @@
+// Library
+import React, { useEffect, useState } from "react";
+import { Global } from '@emotion/react';
+// Queries
+// Styles
+
+import { HeroHeading, HeroSubheading, TierListStyles } from "../../../components/pages/styles/IndividualPageStyle";
+// Scripts
+import { routeLink } from "../../../static/scripts/global";
+
+// Components
+import { useIndividualPageQuery } from "../../../hooks/insurance/useIndividualPageQuery";
+import Hero from "../../../components/Hero";
+import PageHeroForm from "../../../components/Hero/PageHeroForm";
+import FlexedSection from "../../../components/Sections/FlexedSection";
+import Section from "../../../components/Sections";
+import Button from "../../../components/Buttons/Button";
+import RelatedContent from "../../../components/RelatedContent";
+import Cards from "../../../components/Cards";
+import Card from "../../../components/Cards/Card";
+import Footer from "../../../components/Footer";
+import PageHead from "../../../components/PageHead";
+import Layout from "../../../components/Layout";
+
+const IndividualPage = () => {
+    const { page } = useIndividualPageQuery();
+    const { individualSection2 } = page.individualPageCustomField;
+    const [hasChildren, setHasChildren] = useState(false);
+    useEffect(() => {
+        let n = 0;
+        let delay = setInterval(() => {
+            const beContainer = document.getElementById('relatedContent');
+            if (n > 0 && beContainer?.childElementCount > 0) {
+                setHasChildren(true);
+            } else {
+                setHasChildren(false);
+            }
+            if (n >= 5) clearInterval(delay);
+            n++;
+        }, 50);
+    }, []);
+    return (
+        <Layout pageClass='individual'>
+            <Global styles={TierListStyles} />
+            <Hero
+                image={page.pageHeroFields.heroImage.sourceUrl}
+                mobileImage={page.pageHeroFields.mobileHeroImage.sourceUrl}
+                bgColor="#BBD9F1">
+                <HeroHeading>{page.pageHeroFields.headline}</HeroHeading>
+                <HeroSubheading>{page.pageHeroFields.subheadline}</HeroSubheading>
+                <PageHeroForm
+                    btnLeftText={page.pageHeroFields.heroButtons.heroButton1.text}
+                    btnRightText={page.pageHeroFields.heroButtons.heroButton2.text}
+                    inputId="individualPageHeroLocation"
+                    footerContent={page.pageHeroFields.callUs} />
+            </Hero>
+            <FlexedSection
+                color={page.individualPageCustomField.individualSection1.color}
+                heading={page.individualPageCustomField.individualSection1.sectionHeading}>
+                <div className="flex-container center-content">
+                    <p className="sectionPar" dangerouslySetInnerHTML={{ __html: page.individualPageCustomField.individualSection1.text }} />
+                </div>
+            </FlexedSection>
+            <div className='plan' style={{ background: 'rgb(244, 250, 253)' }}>
+                <div className="affordable-plans">
+                    <h1>{page.individualPageCustomField.individualSection2.title}</h1>
+                    <p>{page.individualPageCustomField.individualSection2.statement}</p>
+                </div>
+                <ul className="items hide-at-mobile">
+                    <li style={{ listStylePosition: 'inside', color: '#009FDA' }}>
+                        {individualSection2.bronzePlan.planName}
+                    </li>
+                    <li style={{ listStylePosition: 'outside' }}>
+                        <span style={{ display: 'inline-block', color: '#009FDA' }}>
+                            {individualSection2.bronzePlan.highlightedText}
+                        </span>
+                        {individualSection2.bronzePlan.normalText}
+                    </li>
+                    <li style={{ listStylePosition: 'inside', color: '#009FDA' }}>
+                        {individualSection2.silverPlan.planName}
+                    </li>
+                    <li style={{ listStylePosition: 'outside' }}>
+                        <span style={{ display: 'inline-block', color: '#009FDA' }}>
+                            {individualSection2.silverPlan.highlightedText}
+                        </span>
+                        {individualSection2.silverPlan.normalText}
+                    </li>
+                    <li style={{ listStylePosition: 'inside', color: '#009FDA' }}>
+                        {individualSection2.goldPlan.planName}
+                    </li>
+                    <li style={{ listStylePosition: 'outside', }}>
+                        <span style={{ display: 'inline-block' }}>
+                            {individualSection2.goldPlan.highlightedText}
+                        </span>
+                        <div style={{ color: '#009FDA', }}>
+                            {individualSection2.goldPlan.normalText}
+                        </div>
+                    </li>
+                    <li style={{ listStylePosition: 'inside', color: '#009FDA' }}>
+                        {individualSection2.platinumPlan.planName}
+                    </li>
+                    <li style={{ listStylePosition: 'outside' }}>
+                        <span style={{ display: 'inline-block' }}>
+                            {individualSection2.platinumPlan.highlightedText}
+                        </span>
+                        <div style={{ color: '#009FDA' }}>
+                            {individualSection2.platinumPlan.normalText}
+                        </div>
+                    </li>
+                    <li style={{ listStylePosition: 'inside' }}>
+                        {individualSection2.catastrophicCoverage.planName}
+                    </li>
+                    <li style={{ listStylePosition: 'outside' }} className="end">
+                        <span style={{ display: 'inline-block', color: '#009FDA' }}>
+                            {individualSection2.catastrophicCoverage.highlightedText}
+                        </span>
+                        {individualSection2.catastrophicCoverage.normalText}
+                    </li>
+                    <ul className="items show-at-mobile">
+                        <li>
+                            <div className="dot"></div>
+                            <div className="content">
+                                <h3 style={{ color: '#009FDA' }}>{individualSection2.bronzePlan.planName}</h3>
+                                <p style={{ color: '#009FDA' }}>{individualSection2.bronzePlan.highlightedText}</p>
+                                <p>{individualSection2.bronzePlan.normalText}</p>
+                            </div>
+                            <div className="line"></div>
+                        </li>
+                    </ul>
+                    <li>
+                        <div className="dot"></div>
+                        <div className="content">
+                            <h3 style={{ color: '#009FDA' }}>{individualSection2.silverPlan.planName}</h3>
+                            <p style={{ color: '#009FDA' }}>{individualSection2.silverPlan.highlightedText}</p>
+                            <p>{individualSection2.silverPlan.normalText}</p>
+                        </div>
+                        <div className="line"></div>
+                    </li>
+
+                    <li>
+                        <div className="dot"></div>
+                        <div className="content">
+                            <h3 style={{ color: '#009FDA' }}>{individualSection2.goldPlan.planName}</h3>
+                            <p>{individualSection2.goldPlan.highlightedText}</p>
+                            <p style={{ color: '#009FDA' }}>{individualSection2.goldPlan.normalText}</p>
+                        </div>
+                        <div className="line"></div>
+                    </li>
+                    <li>
+                        <div className="dot"></div>
+                        <div className="content">
+                            <h3 style={{ color: '#009FDA' }}>{individualSection2.platinumPlan.planName}</h3>
+                            <p>{individualSection2.platinumPlan.highlightedText}</p>
+                            <p style={{ color: '#009FDA' }}>{individualSection2.platinumPlan.normalText}</p>
+                        </div>
+                        <div className="line"></div>
+                    </li>
+                    <li>
+                        <div className="dot"></div>
+                        <div className="content">
+                            <h3>{individualSection2.catastrophicCoverage.planName}</h3>
+                            <p style={{ color: '#009FDA' }}>{individualSection2.catastrophicCoverage.highlightedText}</p>
+                            <p>{individualSection2.catastrophicCoverage.normalText}</p>
+                        </div>
+                        <div className="line"></div>
+                    </li>
+                </ul>
+
+                <div className="full-rounded" style={{ textAlign: "center" }}>
+                    <p className="statement">{individualSection2.statement2}</p>
+                    <a href={page.individualPageCustomField.individualSection2.cta.link} onClick={routeLink}>
+                        <Button background="accent-alt" border="light" color="light">
+                            {page.individualPageCustomField.individualSection2.cta.text}
+                        </Button>
+                    </a>
+                </div>
+            </div>
+            <Section
+                color={page.individualPageCustomField.individualSection3.color}
+                heading={page.individualPageCustomField.individualSection3.heading}>
+                <RelatedContent />
+                {(!hasChildren) ? (
+                    <Cards relatedContent={true}>
+                        <Card
+                            image={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent1.image.sourceUrl}
+                            title={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent1.heading}
+                            link={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent1.link}>
+                            <p dangerouslySetInnerHTML={{ __html: page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent1.content }} />
+                        </Card>
+                        <Card
+                            image={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent2.image.sourceUrl}
+                            title={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent2.heading}
+                            link={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent2.link}>
+                            <p dangerouslySetInnerHTML={{ __html: page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent2.content }} />
+                        </Card>
+                        <Card
+                            image={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent3.image.sourceUrl}
+                            title={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent3.heading}
+                            link={page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent3.link}>
+                            <p dangerouslySetInnerHTML={{ __html: page.individualPageCustomField.individualSection3.individualRelatedContent.individualRelatedContent3.content }} />
+                        </Card>
+                    </Cards>
+                ) : null}
+                <div className="full-rounded" style={{ textAlign: "center" }}>
+                    <a href={page.individualPageCustomField.individualSection3.cta.link} onClick={routeLink}>
+                        <Button background="accent-alt" border="light" color="light">
+                            {page.individualPageCustomField.individualSection3.cta.text}
+                        </Button>
+                    </a>
+                </div>
+            </Section>
+            <Footer>
+                {page.disclaimers.disclaimer}
+            </Footer>
+        </Layout >
+    )
+}
+export default IndividualPage
+export const Head = () => {
+    const { page } = useIndividualPageQuery();
+    return (
+        <>
+            <PageHead
+                title={page.seo.title}
+                description={page.seo.metaDesc} />
+            <script src="//cdn.bc0a.com/autopilot/f00000000075672/autopilot_sdk.js"></script>
+        </>
+    )
+}
